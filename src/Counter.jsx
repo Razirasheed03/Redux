87e5477dd60ceed558f12a846a12bfc3d0bfb769
Razirasheed03 <razirasheed03@gmail.com>
2025-04-13 19:13:27 +0530

    initial commit

---
+++ src/Counter.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { decrementCounter, incrementCounter } from './CounterSlice'
+
+const Counter = () => {
+    const count=useSelector((state)=>state.Counter.count)
+    const dispatch=useDispatch()
+  return (
+
+    <div>
+        <h1>{count}</h1>
+        <button onClick={()=>dispatch(incrementCounter())}>+</button>
+        <button onClick={()=>dispatch(decrementCounter())}>-</button>
+    </div>
+  )
+}
+
+export default Counter
---
+++ src/CounterSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialState={
+    count:0
+}
+
+const CounterSlice=createSlice({
+    name:'counter',
+    initialState,
+    reducers:{
+        incrementCounter:(state)=>{
+            state.count=state.count+1
+        },
+        decrementCounter:(state)=>{
+            state.count=state.count-1
+        }
+    }
+
+})
+export const {incrementCounter,decrementCounter}=CounterSlice.actions
+export default CounterSlice.reducer
